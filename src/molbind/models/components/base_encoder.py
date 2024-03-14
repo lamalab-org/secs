eedc7adadffb6bfcdfad780d5f6cf446b9acea13
AdrianM0 <adrianmirza81@gmail.com>
2024-03-14 08:53:55 +0100

    fix: file contents

--- src/molbind/models/components/base_encoder.py
+++ src/molbind/models/components/base_encoder.py
@@ -1,42 +1,22 @@
-from molbind.models.components.base_encoder import BaseModalityEncoder
-from typing import Dict, Literal, Hint
+from typing import Dict, Literal
 from transformers import AutoModelForCausalLM
 import torch.nn as nn
-
-
-class SmilesEncoder(BaseModalityEncoder):
+    
+    
+class BaseModalityEncoder(nn.Module):
     def __init__(self, 
-                projection_head_type=Hint["linear", "non-linear"],
+                projection_head_type : Literal["linear", "non-linear"] = "non-linear",
                 pretrained=True,
                 **kwargs):
-        super().__init__(projection_head_type, pretrained, **kwargs)
-        self.encoder = self.build_encoder()
+       self.pretrained = pretrained
+       self.encoder = self.build_encoder() 
+
     
     def build_encoder(self):
-        return AutoModelForCausalLM.from_pretrained("seyonec/ChemBERTa-zinc-base-v1")
+        pass
     
     def forward(self, x):
         x = self.encoder(x)
-        return x
-    
-    
-class SelfiesEncoder(BaseModalityEncoder):
-    def __init__(self, 
-                projection_head_type=Hint["linear", "non-linear"],
-                pretrained=True,
-                **kwargs):
-        super().__init__(projection_head_type, pretrained, **kwargs)
-        self.encoder = self.build_encoder()
-        self.projection_head = self.build_projection_head(config.projection_parameters)
-        self.pooling_function = "max/min/last/first"
-    
-    def build_encoder(self):
-        return AutoModelForCausalLM.from_pretrained("HUBioDataLab/SELFormer")
-    
-    def forward(self, x):
-        x = self.encoder(x)
-        # pool embeddings 
-        
         x = self.projection_head(x)
         # pooling
         return x
--- src/molbind/models/components/custom_encoders.py
+++ src/molbind/models/components/custom_encoders.py
@@ -1,27 +1,44 @@
 import torch.nn as nn
-from typing import Dict, Literal, Hint
+from typing import Dict, Literal
 from molbind.models.components.head import ProjectionHead
+from molbind.models.components.base_encoder import BaseModalityEncoder
+from transformers import AutoModelForCausalLM
 
 
-class BaseModalityEncoder(nn.Module):
+class SmilesEncoder(BaseModalityEncoder):
     def __init__(self, 
-                projection_head_type : Hint["linear", "non-linear"] = "non-linear",
+                projection_head_type=Literal["linear", "non-linear"],
                 pretrained=True,
                 **kwargs):
-       self.pretrained = pretrained
-        
-        # some from HuggingFace, others from scratch
-        # restore weights = True/False
-        # freeze weights = True/False
-        # projection head type = linear/non-linear
-        self.encoder = self.build_encoder() 
-
+        super().__init__(projection_head_type, pretrained, **kwargs)
+        self.encoder = self.build_encoder()
     
     def build_encoder(self):
-        pass
+        return AutoModelForCausalLM.from_pretrained("seyonec/ChemBERTa-zinc-base-v1")
     
     def forward(self, x):
         x = self.encoder(x)
+        return x
+    
+    
+class SelfiesEncoder(BaseModalityEncoder):
+    def __init__(self, 
+                freeze_encoder: bool = False,
+                pretrained=True,
+                **kwargs):
+        super().__init__(projection_head_type, pretrained, **kwargs)
+        self.encoder = self.build_encoder()
+    
+    def build_encoder(self):
+        if self.pretrained:
+            return AutoModelForCausalLM.from_pretrained("seyonec/ChemBERTa-zinc-base-v1")
+        else:
+            return AutoModelForCausalLM.from_config(self.config)
+    
+    def forward(self, x):
+        x = self.encoder(x)
+        # pool embeddings 
+        
         x = self.projection_head(x)
         # pooling
         return x
