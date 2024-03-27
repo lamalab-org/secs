26ed4407410d6515216701f7445281f4adf39923
AdrianM0 <adrianmirza81@gmail.com>
2024-03-27 14:53:27 +0100

    first version of how to deal with multimodal embeddings (variable nr of embeddings)

--- src/molbind/models/model.py
+++ src/molbind/models/model.py
@@ -1,21 +1,38 @@
-from molbind.models.components.custom_encoders import SmilesEncoder, SelfiesEncoder
+from molbind.models.components.custom_encoders import SmilesEncoder, SelfiesEncoder, GraphEncoder
 from molbind.models.components.head import ProjectionHead
 from molbind.models.component.pooler import EmbedPooler
 from molbind.data.dataloaders import MolBindDataModule
 import pytorch_lightning as pl
+from torch import Tensor
 
 
 class MolBind(nn.Module):
-    def __init__(self, model_opt_params):
+    def __init__(self, cfg):
         super(MolBind, self).__init__()
-        self.smiles_encoder = SmilesEncoder()
-        self.selfies_encoder = SelfiesEncoder()
-        self.projection_head = ProjectionHead()
-        self.pooler = EmbedPooler()
+        # Instantiate all encoders
+        self.smiles_encoder = SmilesEncoder(cfg.model.smiles_encoder)
+        self.selfies_encoder = SelfiesEncoder(cfg.model.selfies_encoder)
+        self.graph_encoder = GraphEncoder(cfg.model.graph_encoder)
+        self.nmr_encoder = None
         
-    def forward(self, input_ids_modality_1, attention_mask_modality_1, input_ids_modality_2, attention_mask_modality_2):
-        smiles_embedding = self.smiles_encoder(input_ids_modality_1, attention_mask_modality_1)
-        selfies_embedding = self.selfies_encoder(input_ids_modality_2, attention_mask_modality_2)
-        joint_embedding = self.projection_head(smiles_embedding, selfies_embedding)
-        return joint_embedding
+        self.dict_encoders = {
+            "smiles" : self.smiles_encoder,
+            "selfies" : self.selfies_encoder,
+            "graph" : self.graph_encoder,
+            "nmr" : self.nmr_encoder
+        }
         
+        # Instantiate projection head and pooler
+        self.pooler = EmbedPooler(cfg.model.pooler)
+        self.projection_head = ProjectionHead(cfg.model.projection_head)
+        
+    def forward(self, input) -> Tensor:
+        store_embeddings = {}
+        # dataloader keys are other modalities
+        for modality in input.keys():
+            # forward through respective encoder
+            store_embeddings[modality] = self.dict_encoders[modality](input[modality]).last_hidden_state
+            # projection head
+            store_embeddings[modality] = self.projection_head(store_embeddings[modality])
+        return store_embeddings
+
