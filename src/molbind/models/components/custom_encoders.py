babda3a28d41021de0ff9ac78580de43e0bde62a
AdrianM0 <adrianmirza81@gmail.com>
2024-04-08 09:24:54 +0200

    feat: clean-up encoderes

--- src/molbind/models/components/custom_encoders.py
+++ src/molbind/models/components/custom_encoders.py
@@ -1,8 +1,8 @@
-import torch.nn as nn
-from typing import Dict, Literal
-from molbind.models.components.base_encoder import BaseModalityEncoder
+from typing import Dict
+import torch
 from transformers import AutoModelForCausalLM
-from  molbind.utils.utils import reinitialize_weights
+from molbind.models.components.base_encoder import BaseModalityEncoder
+from molbind.utils.utils import reinitialize_weights
 
 
 class SmilesEncoder(BaseModalityEncoder):
@@ -28,10 +28,7 @@ class SmilesEncoder(BaseModalityEncoder):
 
 
 class SelfiesEncoder(BaseModalityEncoder):
-    def __init__(self,
-                freeze_encoder: bool = False,
-                pretrained=True,
-                **kwargs) -> None:
+    def __init__(self, freeze_encoder: bool = False, pretrained=True, **kwargs) -> None:
         super().__init__(freeze_encoder, pretrained, **kwargs)
         self.encoder = AutoModelForCausalLM.from_pretrained("HUBioDataLab/SELFormer")
         if freeze_encoder:
@@ -40,7 +37,7 @@ class SelfiesEncoder(BaseModalityEncoder):
         if not pretrained:
             self.encoder = reinitialize_weights(self.encoder)
 
-    def forward(self, x):
+    def forward(self, x : Dict[str, torch.Tensor]) -> torch.Tensor:
         token_ids, attention_mask = x
 
         return self.encoder(
@@ -54,21 +51,43 @@ class IUPACNameEncoder(BaseModalityEncoder):
     pass
 
 
-class GraphEncoder(BaseModalityEncoder):
-    def __init__(self, 
-                freeze_encoder: bool = False,
-                pretrained: bool = False,
-                **kwargs) -> None:
+class IREncoder(BaseModalityEncoder):
+    def __init__(self, freeze_encoder: bool = False, pretrained: bool = False, **kwargs) -> None:
         super().__init__(freeze_encoder, pretrained, **kwargs)
-        self.encoder = self.build_encoder()
+        self.encoder = None
         if freeze_encoder:
             for param in self.encoder.parameters():
                 param.requires_grad = False
         if pretrained:
             self.encoder = reinitialize_weights(self.encoder)
 
-    def build_encoder(self):
-        return nn.Identity()
-
     def forward(self, x):
-        return self.encoder(x[0], x[1])
+        token_ids, attention_mask = x
+        return self.encoder(
+            input_ids=token_ids,
+            attention_mask=attention_mask,
+            output_hidden_states=True
+            ).hidden_states[-1].sum(dim=1)/attention_mask.sum(dim=1).unsqueeze(1)
+
+
+class GraphEncoder(BaseModalityEncoder):
+    pass
+
+
+class NMREncoder(BaseModalityEncoder):
+    def __init__(self, freeze_encoder: bool = False, pretrained=True, **kwargs):
+        super().__init__(freeze_encoder, pretrained, **kwargs)
+        self.encoder = None
+        if freeze_encoder:
+            for param in self.encoder.parameters():
+                param.requires_grad = False
+        if not pretrained:
+            self.encoder = reinitialize_weights(self.encoder)
+    
+    def forward(self, x):
+        token_ids, attention_mask = x
+        return self.encoder(
+            input_ids=token_ids,
+            attention_mask=attention_mask,
+            output_hidden_states=True
+            ).hidden_states[-1].sum(dim=1)/attention_mask.sum(dim=1).unsqueeze(1)
