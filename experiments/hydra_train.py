48f28b1fa90c5ee7fade6bb6e51935b4e4062937
AdrianM0 <adrianmirza81@gmail.com>
2024-04-11 08:36:49 +0200

    feat : hydra training

---
+++ experiments/hydra_train.py
@@ -0,0 +1,89 @@
+import hydra
+import pytorch_lightning as L
+import polars as pl
+from molbind.data.dataloaders import load_combined_loader
+from molbind.models.lightning_module import MolBindModule
+from omegaconf import DictConfig
+import torch
+
+
+def train_molbind(config: DictConfig):
+    wandb_logger = L.loggers.WandbLogger(**config.logger.wandb)
+
+    device_count = torch.cuda.device_count()
+    trainer = L.Trainer(
+        max_epochs=100,
+        accelerator="cuda",
+        log_every_n_steps=10,
+        logger=wandb_logger,
+        devices=device_count if device_count > 1 else "auto",
+        strategy="ddp" if device_count > 1 else "auto",
+    )
+
+    train_modality_data = {}
+    valid_modality_data = {}
+
+    # Example SMILES - SELFIES modality pair:
+    data = pl.read_csv(config.data.dataset_path)
+    shuffled_data = data.sample(
+        fraction=config.data.fraction_data, shuffle=True, seed=config.data.seed
+    )
+    dataset_length = len(shuffled_data)
+    valid_shuffled_data = shuffled_data.tail(
+        int(config.data.valid_frac * dataset_length)
+    )
+    train_shuffled_data = shuffled_data.head(
+        int(config.data.train_frac * dataset_length)
+    )
+
+    columns = shuffled_data.columns
+    # extract non-central modalities (i.e. not the central modality smiles)
+    non_central_modalities = config.data.modalities
+
+    for column in columns:
+        if column in non_central_modalities:
+            # drop nan for specific pair
+            train_modality_smiles_pair = train_shuffled_data[
+                ["smiles", column]
+            ].drop_nulls()
+            valid_modality_smiles_pair = valid_shuffled_data[
+                ["smiles", column]
+            ].drop_nulls()
+
+            train_modality_data[column] = [
+                train_modality_smiles_pair["smiles"].to_list(),
+                train_modality_smiles_pair[column].to_list(),
+            ]
+            valid_modality_data[column] = [
+                valid_modality_smiles_pair["smiles"].to_list(),
+                valid_modality_smiles_pair[column].to_list(),
+            ]
+
+    combined_loader = load_combined_loader(
+        data_modalities=train_modality_data,
+        batch_size=config.data.batch_size,
+        shuffle=True,
+        num_workers=1,
+    )
+
+    valid_dataloader = load_combined_loader(
+        data_modalities=valid_modality_data,
+        batch_size=config.data.batch_size,
+        shuffle=False,
+        num_workers=1,
+    )
+
+    trainer.fit(
+        MolBindModule(config),
+        train_dataloaders=combined_loader,
+        val_dataloaders=valid_dataloader,
+    )
+
+
+@hydra.main(config_path="../configs", config_name="train.yaml")
+def main(config: DictConfig):
+    train_molbind(config)
+
+
+if __name__ == "__main__":
+    main()
--- src/molbind/models/lightning_module.py
+++ src/molbind/models/lightning_module.py
@@ -2,10 +2,6 @@ from info_nce import InfoNCE
 from pytorch_lightning import LightningModule
 from molbind.models.model import MolBind
 import torch
-import pytorch_lightning as L
-from molbind.data.dataloaders import load_combined_loader
-from typing import Dict
-import polars as pl
 
 
 class MolBindModule(LightningModule):
@@ -43,78 +39,3 @@ class MolBindModule(LightningModule):
             lr=self.config.model.optimizer.lr,
             weight_decay=self.config.model.optimizer.weight_decay,
         )
-
-
-# def train_molbind(config: Dict = None):
-#     wandb_logger = L.loggers.WandbLogger(
-#         project=config.wandb.project_name, entity=config.wandb.entity
-#     )
-
-#     device_count = torch.cuda.device_count()
-#     trainer = L.Trainer(
-#         max_epochs=100,
-#         accelerator="cuda",
-#         log_every_n_steps=10,
-#         logger=wandb_logger,
-#         devices=device_count if device_count > 1 else "auto",
-#         strategy="ddp" if device_count > 1 else "auto",
-#     )
-
-#     train_modality_data = {}
-#     valid_modality_data = {}
-
-#     data = pl.read_csv(config.data.dataset_path)
-#     shuffled_data = data.sample(
-#         fraction=config.data.fraction_data, shuffle=True, seed=config.data.seed
-#     )
-#     dataset_length = len(shuffled_data)
-#     valid_shuffled_data = shuffled_data.tail(
-#         int(config.data.valid_frac * dataset_length)
-#     )
-#     train_shuffled_data = shuffled_data.head(
-#         int(config.data.train_frac * dataset_length)
-#     )
-
-#     columns = shuffled_data.columns
-#     # extract non-central modalities (i.e. not the central modality smiles)
-#     non_central_modalities = config.data.modalities
-#     central_modality = config.data.central_modality
-
-#     for column in columns:
-#         if column in non_central_modalities:
-#             # drop nan for specific pair
-#             train_modality_pair = train_shuffled_data[
-#                 [central_modality, column]
-#             ].drop_nulls()
-#             valid_modality_pair = valid_shuffled_data[
-#                 [central_modality, column]
-#             ].drop_nulls()
-
-#             train_modality_data[column] = [
-#                 train_modality_pair[central_modality].to_list(),
-#                 train_modality_pair[column].to_list(),
-#             ]
-#             valid_modality_data[column] = [
-#                 valid_modality_pair[central_modality].to_list(),
-#                 valid_modality_pair[column].to_list(),
-#             ]
-
-#     combined_loader = load_combined_loader(
-#         data_modalities=train_modality_data,
-#         batch_size=config.data.batch_size,
-#         shuffle=True,
-#         num_workers=1,
-#     )
-
-#     valid_dataloader = load_combined_loader(
-#         data_modalities=valid_modality_data,
-#         batch_size=config.data.batch_size,
-#         shuffle=False,
-#         num_workers=1,
-#     )
-
-#     trainer.fit(
-#         MolBindModule(config),
-#         train_dataloaders=combined_loader,
-#         val_dataloaders=valid_dataloader,
-#     )
