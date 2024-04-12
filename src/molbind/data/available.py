377341f57092a2a51af26e031cf4a51a8dbe4e5e
AdrianM0 <adrianmirza81@gmail.com>
2024-04-12 11:16:40 +0200

    refactor a bit the code

---
+++ src/molbind/data/available.py
@@ -0,0 +1,34 @@
+from networkx import Graph
+
+from molbind.data.components.tokenizers import SELFIES_TOKENIZER, SMILES_TOKENIZER
+from molbind.models.components.base_encoder import FingerprintEncoder
+from molbind.models.components.custom_encoders import (
+    GraphEncoder,
+    SelfiesEncoder,
+    SmilesEncoder,
+)
+
+AVAILABLE_ENCODERS = {
+    "smiles": SmilesEncoder,
+    "selfies": SelfiesEncoder,
+    "graph": GraphEncoder,
+    "nmr": None,
+    "fingerprint": FingerprintEncoder,
+}
+
+
+MODALITY_DATA_TYPES = {
+    "fingerprint": list,
+    "iupac_name": str,
+    "smiles": str,
+    "selfies": str,
+    "graph": Graph,
+    "nmr": str,
+    "ir": str,
+}
+
+STRING_TOKENIZERS = {
+    "smiles": SMILES_TOKENIZER,
+    "selfies": SELFIES_TOKENIZER,
+    "iupac_name": "iupac_name_tokenizer",
+}
--- src/molbind/data/dataloaders.py
+++ src/molbind/data/dataloaders.py
@@ -1,84 +1,9 @@
-from torch.utils.data import DataLoader, Dataset
 from lightning.pytorch.utilities.combined_loader import CombinedLoader
-from molbind.data.components.tokenizers import SMILES_TOKENIZER, SELFIES_TOKENIZER
 from networkx import Graph
-from typing import Tuple, Optional
-from torch import Tensor
+from torch.utils.data import DataLoader
 
-
-MODALITY_DATA_TYPES = {
-    "smiles": str,
-    "selfies": str,
-    "graph": Graph,
-    "nmr": str,
-    "ir": str,
-}
-
-STRING_TOKENIZERS = {
-    "smiles": SMILES_TOKENIZER,
-    "selfies": SELFIES_TOKENIZER,
-    "iupac_name": "iupac_name_tokenizer",
-}
-
-
-class StringDataset(Dataset):
-    def __init__(
-        self,
-        dataset: Tuple[Tensor, Tensor],
-        modality: str,
-        central_modality: str = "smiles",
-        context_length: Optional[int] = 256,
-    ):
-        """Dataset for string modalities.
-
-        Args:
-            dataset (Tuple[Tensor, Tensor]): pair of SMILES and data for the modality (smiles always index 0, modality index 1)
-            modality (str): name of data modality as found in MODALITY_DATA_TYPES
-            context_length (int, optional): _description_. Defaults to 256.
-        """
-        assert len(dataset) == 2
-        assert len(dataset[0]) == len(dataset[1])
-
-        self.modality = modality
-        self.central_modality = central_modality
-
-        assert MODALITY_DATA_TYPES[modality] == str
-        assert MODALITY_DATA_TYPES[central_modality] == str
-
-        self.tokenized_central_modality = STRING_TOKENIZERS[central_modality](
-            dataset[0],
-            padding="max_length",
-            truncation=True,
-            return_tensors="pt",
-            max_length=context_length,
-        )
-
-        self.tokenized_string = STRING_TOKENIZERS[modality](
-            dataset[1],
-            padding="max_length",
-            truncation=True,
-            return_tensors="pt",
-            max_length=context_length,
-        )
-
-    def __len__(self):
-        return len(self.tokenized_central_modality.input_ids)
-
-    def __getitem__(self, idx):
-        return {
-            self.central_modality: (
-                self.tokenized_central_modality.input_ids[idx],
-                self.tokenized_central_modality.attention_mask[idx],
-            ),
-            self.modality: (
-                self.tokenized_string.input_ids[idx],
-                self.tokenized_string.attention_mask[idx],
-            ),
-        }
-
-
-class GraphDataset(Dataset):
-    pass
+from molbind.data.available import MODALITY_DATA_TYPES
+from molbind.data.components.datasets import GraphDataset, StringDataset
 
 
 def load_combined_loader(
