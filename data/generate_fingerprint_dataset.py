9aca39934e981986e2f5b4ee19e86d7bc4683d85
AdrianM0 <adrianmirza81@gmail.com>
2024-04-19 15:09:11 +0200

    typing fix and docstring

--- data/generate_fingerprint_dataset.py
+++ data/generate_fingerprint_dataset.py
@@ -1,21 +1,23 @@
 import fire
 import pandas as pd
+
 from molbind.data.utils.fingerprint_utils import get_morgan_fingerprint_from_smiles
 
 
 def add_fingerprint_column_to_dataframe(
     csv_data_path: str, radius: int = 4, nbits: int = 2048
-):
+) -> None:
     """
-    Add fingerprint column to a DataFrame
+    Add fingerprint column to a pandas dataframe. The dataframe is saved as a
+    pickle file with the same name as the csv_data_path (str) input variable,
+    but with a changed extension.
 
     Args:
-        csv_data_path (Union[Path, str]): Path to csv file with data
+        csv_data_path (str): Path to csv file with data
         radius (int, optional): radius of the fingerprint. Defaults to 2.
         nbits (int, optional): number of bits in the fingerprint. Defaults to 2048.
-
     Returns:
-        pl.DataFrame: DataFrame with fingerprint column
+        None
     """
 
     # Load data
@@ -23,7 +25,7 @@ def add_fingerprint_column_to_dataframe(
 
     # Add fingerprint column
     data["fingerprint"] = data["smiles"].apply(
-        lambda x: get_morgan_fingerprint_from_smiles(x, radius, nbits)
+        lambda smi: get_morgan_fingerprint_from_smiles(smi, radius, nbits)
     )
 
     # Save data to pkl
