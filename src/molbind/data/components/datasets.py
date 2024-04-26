db53b91f6e872ec3854f98c03ffc54ceb5173898
AdrianM0 <adrianmirza81@gmail.com>
2024-04-26 15:14:33 +0200

    feat: graph dataset suitable for molbind training :llama:

--- src/molbind/data/components/datasets.py
+++ src/molbind/data/components/datasets.py
@@ -1,7 +1,21 @@
-from typing import Tuple  # noqa: UP035, I002
+import math  # noqa: I002
+import random
+from copy import deepcopy
+from typing import List, Tuple, Union  # noqa: UP035
 
+import numpy as np
+import torch
+from rdkit import Chem
 from torch import Tensor
 from torch.utils.data import Dataset
+from torch_geometric.data import Data
+
+from molbind.data.utils.graph_utils import (
+    ATOM_LIST,
+    BOND_LIST,
+    BONDDIR_LIST,
+    CHIRALITY_LIST,
+)
 
 
 class StringDataset(Dataset):
@@ -77,7 +91,119 @@ class FingerprintMolBindDataset(Dataset):
 
 
 class GraphDataset(Dataset):
-    pass
+    def __init__(
+        self,
+        graph_data: List[str], # noqa: UP006
+        central_modality: str,
+        central_modality_data: Union[Tensor, Tuple[Tensor, Tensor]], # noqa: UP006
+    ) -> None:
+        super().__init__()
+        self.central_modality = central_modality
+        self.modality = "graph"
+        self.smiles_data = graph_data[self.modality]
+        self.central_modality_data = central_modality_data
+
+    def __len__(self):
+        return len(self.smiles_data)
+
+    def __getitem__(self, index: int) -> Tuple:  # noqa: UP006
+        mol = Chem.MolFromSmiles(self.smiles_data[index])
+        # mol = Chem.AddHs(mol)
+
+        N = mol.GetNumAtoms()
+        M = mol.GetNumBonds()
+
+        type_idx = []
+        chirality_idx = []
+        atomic_number = []
+        # aromatic = []
+        # sp, sp2, sp3, sp3d = [], [], [], []
+        # num_hs = []
+        for atom in mol.GetAtoms():
+            type_idx.append(ATOM_LIST.index(atom.GetAtomicNum()))
+            chirality_idx.append(CHIRALITY_LIST.index(atom.GetChiralTag()))
+            atomic_number.append(atom.GetAtomicNum())
+            # aromatic.append(1 if atom.GetIsAromatic() else 0)
+            # hybridization = atom.GetHybridization()
+            # sp.append(1 if hybridization == HybridizationType.SP else 0)
+            # sp2.append(1 if hybridization == HybridizationType.SP2 else 0)
+            # sp3.append(1 if hybridization == HybridizationType.SP3 else 0)
+            # sp3d.append(1 if hybridization == HybridizationType.SP3D else 0)
+
+        # z = torch.tensor(atomic_number, dtype=torch.long)
+        x1 = torch.tensor(type_idx, dtype=torch.long).view(-1, 1)
+        x2 = torch.tensor(chirality_idx, dtype=torch.long).view(-1, 1)
+        x = torch.cat([x1, x2], dim=-1)
+        # x2 = torch.tensor([atomic_number, aromatic, sp, sp2, sp3, sp3d, num_hs],
+        #                     dtype=torch.float).t().contiguous()
+        # x = torch.cat([x1.to(torch.float), x2], dim=-1)
+
+        row, col, edge_feat = [], [], []
+        for bond in mol.GetBonds():
+            start, end = bond.GetBeginAtomIdx(), bond.GetEndAtomIdx()
+            row += [start, end]
+            col += [end, start]
+            # edge_type += 2 * [MOL_BONDS[bond.GetBondType()]]
+            edge_feat.append(
+                [
+                    BOND_LIST.index(bond.GetBondType()),
+                    BONDDIR_LIST.index(bond.GetBondDir()),
+                ]
+            )
+            edge_feat.append(
+                [
+                    BOND_LIST.index(bond.GetBondType()),
+                    BONDDIR_LIST.index(bond.GetBondDir()),
+                ]
+            )
+
+        edge_index = torch.tensor([row, col], dtype=torch.long)
+        edge_attr = torch.tensor(np.array(edge_feat), dtype=torch.long)
+
+        # random mask a subgraph of the molecule
+        num_mask_nodes = max([1, math.floor(0.25 * N)])
+        num_mask_edges = max([0, math.floor(0.25 * M)])
+        mask_nodes_i = random.sample(list(range(N)), num_mask_nodes)
+        mask_nodes_j = random.sample(list(range(N)), num_mask_nodes)
+        mask_edges_i_single = random.sample(list(range(M)), num_mask_edges)
+        mask_edges_j_single = random.sample(list(range(M)), num_mask_edges)
+        mask_edges_i = [2 * i for i in mask_edges_i_single] + [
+            2 * i + 1 for i in mask_edges_i_single
+        ]
+        mask_edges_j = [2 * i for i in mask_edges_j_single] + [
+            2 * i + 1 for i in mask_edges_j_single
+        ]
+
+        x_i = deepcopy(x)
+        for atom_idx in mask_nodes_i:
+            x_i[atom_idx, :] = torch.tensor([len(ATOM_LIST), 0])
+        edge_index_i = torch.zeros((2, 2 * (M - num_mask_edges)), dtype=torch.long)
+        edge_attr_i = torch.zeros((2 * (M - num_mask_edges), 2), dtype=torch.long)
+        count = 0
+        for bond_idx in range(2 * M):
+            if bond_idx not in mask_edges_i:
+                edge_index_i[:, count] = edge_index[:, bond_idx]
+                edge_attr_i[count, :] = edge_attr[bond_idx, :]
+                count += 1
+        data_i = Data(x=x_i, edge_index=edge_index_i, edge_attr=edge_attr_i)
+
+        x_j = deepcopy(x)
+        for atom_idx in mask_nodes_j:
+            x_j[atom_idx, :] = torch.tensor([len(ATOM_LIST), 0])
+        edge_index_j = torch.zeros((2, 2 * (M - num_mask_edges)), dtype=torch.long)
+        edge_attr_j = torch.zeros((2 * (M - num_mask_edges), 2), dtype=torch.long)
+        count = 0
+        for bond_idx in range(2 * M):
+            if bond_idx not in mask_edges_j:
+                edge_index_j[:, count] = edge_index[:, bond_idx]
+                edge_attr_j[count, :] = edge_attr[bond_idx, :]
+                count += 1
+        data_j = Data(x=x_j, edge_index=edge_index_j, edge_attr=edge_attr_j)
+
+        return {
+            self.central_modality: [i[index] for i in self.central_modality_data],
+            self.modality: [data_i, data_j],
+        }
 
 
 class FingerprintVAEDataset(Dataset):
