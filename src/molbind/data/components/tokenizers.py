197931a2964dae2f1ce9c9d065fab0f9b1cdcdb1
AdrianM0 <adrianmirza81@gmail.com>
2024-05-03 07:43:35 -0700

    lint (import): add ruff noqa

--- src/molbind/data/components/tokenizers.py
+++ src/molbind/data/components/tokenizers.py
@@ -1,4 +1,4 @@
-from transformers import AutoTokenizer
+from transformers import AutoTokenizer  # noqa: I002
 
 SMILES_TOKENIZER = AutoTokenizer.from_pretrained("seyonec/ChemBERTa-zinc-base-v1")
 SELFIES_TOKENIZER = AutoTokenizer.from_pretrained("HUBioDataLab/SELFormer")
