97430a29b13ac740574255b3d197e16fd32cebf4
AdrianM0 <adrianmirza81@gmail.com>
2024-05-23 13:15:42 +0200

    fix (job name): remove file suffix

--- experiments/submit_experiments.sh
+++ experiments/submit_experiments.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 # List of configuration names
-CONFIG_NAMES=("smi_graph.yaml", "smi_graph_sf.yaml", "smi_sf_fps_graph.yaml")
+CONFIG_NAMES=(smi_graph.yaml smi_graph_sf.yaml smi_sf_fps_graph.yaml)
 
 # Submit jobs
 for CONFIG in "${CONFIG_NAMES[@]}"; do
+    echo "Submitting job for config: ${CONFIG}"
     sbatch <<EOT
 #!/bin/bash
-#SBATCH --job-name=molbind_${CONFIG}
+#SBATCH --job-name=molbind_${CONFIG::-5}
 #SBATCH --partition=gpu
 #SBATCH --gres=gpu:1
 #SBATCH --time=72:00:00
