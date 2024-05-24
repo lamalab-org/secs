669a12c7e5bfb903e2567e6fe50e8291ca4e1dc7
AdrianM0 <adrianmirza81@gmail.com>
2024-05-24 11:24:49 +0200

    feat (data): gen pkl file

--- data/gen_pkl_sf_smi_graph_fps.py
+++ data/gen_pkl_sf_smi_graph_fps.py
@@ -1,8 +1,24 @@
-import fire
+import fire  # noqa: I002
 import pandas as pd
+import selfies as sf
 
 from molbind.data.utils.fingerprint_utils import get_morgan_fingerprint_from_smiles
 
+
+def smiles_to_selfies(smiles: str) -> str:
+    """
+    Convert a SELFIES string to a SMILES string.
+
+    Args:
+        selfies (str): SELFIES string
+
+    Returns:
+        str: SMILES string
+    """
+    try:
+        return sf.encoder(smiles)
+    except Exception:
+        return None
 
 def add_fingerprint_column_to_dataframe(
     csv_data_path: str, radius: int = 4, nbits: int = 2048
@@ -22,12 +38,17 @@ def add_fingerprint_column_to_dataframe(
 
     # Load data
     data = pd.read_csv(csv_data_path)
-
+    data = data[["smiles"]]
+    data["selfies"] = data["smiles"].apply(lambda x: smiles_to_selfies(x))
+    print("Finished SELFIES conversion")
     # Add fingerprint column
     data["fingerprint"] = data["smiles"].apply(
         lambda smi: get_morgan_fingerprint_from_smiles(smi, radius, nbits)
     )
 
+
+    data["graph"] = data["smiles"].apply(lambda x: x)
+    data = data.dropna()
     # Save data to pkl
     data.to_pickle(csv_data_path.replace(".csv", ".pkl"))
 
