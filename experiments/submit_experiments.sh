58295622f174dc215c7fab34d830024a5e08fc98
AdrianM0 <adrianmirza81@gmail.com>
2024-05-30 17:10:00 +0200

    chore: update submission script

--- experiments/submit_experiments.sh
+++ experiments/submit_experiments.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 # List of configuration names
-CONFIG_NAMES=(smi_graph.yaml smi_graph_sf.yaml smi_sf_fps_graph.yaml)
+# CONFIG_NAMES=(smi_graph.yaml smi_graph_sf.yaml smi_sf_fps_graph.yaml)
+# CONFIG_NAMES=(smi_graph_256.yaml smi_graph_sf_256.yaml smi_sf_fps_graph_256.yaml)
+CONFIG_NAMES=(smi_graph_1024.yaml)
 
 # Submit jobs
 for CONFIG in "${CONFIG_NAMES[@]}"; do
@@ -9,9 +11,10 @@ for CONFIG in "${CONFIG_NAMES[@]}"; do
 #!/bin/bash
 #SBATCH --job-name=molbind_${CONFIG::-5}
 #SBATCH --partition=gpu
-#SBATCH --gres=gpu:1
+#SBATCH --gres=gpu:3
+#SBATCH --nodelist=gpu014
 #SBATCH --time=72:00:00
-#SBATCH --mem=100GB
+#SBATCH --mem=400GB
 
 source ~/miniconda3/bin/activate
 conda activate molbind
