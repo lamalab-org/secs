a8c4044fd906cced491201020706c0ccfb4d21ae
AdrianM0 <adrianmirza81@gmail.com>
2024-05-30 17:04:21 +0200

    feat: add predict dataloader (adapted for a single loader)

--- src/molbind/data/datamodule.py
+++ src/molbind/data/datamodule.py
@@ -2,6 +2,7 @@ from typing import Dict  # noqa: UP035, I002
 
 from lightning.pytorch.utilities.combined_loader import CombinedLoader
 from pytorch_lightning import LightningDataModule
+from pytorch_lightning.utilities.types import EVAL_DATALOADERS
 from torch.utils.data import DataLoader, DistributedSampler
 from torch_geometric.loader import DataLoader as GeometricDataLoader
 
@@ -17,7 +18,7 @@ class MolBindDataModule(LightningDataModule):
         # create attributes for each subset
         # and add dataloader arguments
         self.datasets = {}
-        for subset in ["train", "val", "test"]:
+        for subset in ["train", "val", "test", "predict"]:
             if subset in data:
                 self.datasets[subset] = data[subset]
         self.dataloader_arguments = data["dataloader_arguments"]
@@ -82,6 +83,50 @@ class MolBindDataModule(LightningDataModule):
         )
         return CombinedLoader(val_dataloaders, "sequential")
 
-    def test_dataloader(self) -> CombinedLoader:
+    def predict_dataloader(self) -> CombinedLoader:
         # iter through test data loaders
-        return self.test_dataloaders if hasattr(self, "test_dataloaders") else None
+        test_dataloaders = self.build_predict_dataloader(
+            batch_size=self.dataloader_arguments["batch_size"],
+            drop_last=True,
+            shuffle=False,
+            num_workers=self.dataloader_arguments["num_workers"],
+            mode="predict",
+        )
+        return CombinedLoader(test_dataloaders, "sequential")
+
+    def build_predict_dataloader(
+        self,
+        batch_size: int,
+        drop_last: bool,
+        shuffle: bool,
+        num_workers: int,
+        mode: str,
+    ) -> CombinedLoader:
+        """
+        Build dataloaders for the predict step.
+        This function is similar to `build_multimodal_dataloader` but does not use the DistributedSampler.
+        Hence, it can be ran on a single GPU.
+
+        After in the `retrieval.py` script the predictions are concatenated.
+        """
+        dataloaders = {}
+        for modality in self.datasets[mode][0]:
+            if modality == NonStringModalities.GRAPH:
+                dataloaders[modality] = GeometricDataLoader(
+                    self.datasets[mode][0][modality],
+                    batch_size=batch_size,
+                    num_workers=num_workers,
+                    drop_last=drop_last,
+                    shuffle=shuffle,
+                )
+            else:
+                dataloaders[modality] = DataLoader(
+                    self.datasets[mode][0][modality],
+                    batch_size=batch_size,
+                    num_workers=num_workers,
+                    drop_last=drop_last,
+                    shuffle=shuffle,
+                )
+        # CombinedLoader does not work with DDPSampler directly
+        # So each dataloader has a DistributedSampler
+        return dataloaders
