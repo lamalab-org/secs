24f5e962e507a26eb55420c65a6b6f2ff096151f
AdrianM0 <adrianmirza81@gmail.com>
2024-08-29 07:25:46 +0200

    fix small bug

--- app/df_app.py
+++ app/df_app.py
@@ -1,4 +1,4 @@
-import contextlib
+# import contextlib
 import os
 
 import pandas as pd
@@ -10,7 +10,7 @@ from rdkit.Chem import Draw
 # Function to save RDKit molecule images
 def save_molecule_image(smiles, file_path):
     mol = Chem.MolFromSmiles(smiles)
-    img = Draw.MolToImage(mol, width=900)
+    img = Draw.MolToImage(mol, width=600)
     img.save(file_path)
     return file_path
 
@@ -73,77 +73,95 @@ if uploaded_file is not None:
         value=int(data["Unique Carbons"].max()),
     )
 
-    col1, col2, col3 = st.columns([1, 3, 1])
-    with col1:
-        st.write("Not synthetically accessible")
-    with col2:
-        synthetic_quantile = st.slider(
-            "Synthetic Accessibility Quantile:",
-            min_value=0.0,
-            max_value=1.0,
-            value=0.85,
-        )
-    with col3:
-        st.write("Synthetically accessible")
+    # col1, col2, col3 = st.columns([1, 3, 1])
+    # with col1:
+    #     st.write("Not synthetically accessible")
+    # with col2:
+    #     synthetic_quantile = st.slider(
+    #         "Synthetic Accessibility Quantile:",
+    #         min_value=0.0,
+    #         max_value=1.0,
+    #         value=0.85,
+    #     )
+    # with col3:
+    #     st.write("Synthetically accessible")
+    tanimoto_1_df = data[data["Tanimoto Similarity"] == 1]
 
-
-    with contextlib.suppress(Exception):
+    # with contextlib.suppress(Exception):
         # Apply filters
-        df_filtered = data[
-            (data["Unique Hydrogens"] >= min_hydrogens)
-            & (data["Unique Hydrogens"] <= max_hydrogens)
-            & (data["Unique Carbons"] >= min_carbons)
-            & (data["Unique Carbons"] <= max_carbons)
-            & (data["Synthetic Accessibility Score"] > data["Synthetic Accessibility Score"].quantile(1 - synthetic_quantile))
-        ]
+    df_filtered = data[
+        (data["Unique Hydrogens"] >= min_hydrogens)
+        & (data["Unique Hydrogens"] <= max_hydrogens)
+        & (data["Unique Carbons"] >= min_carbons)
+        & (data["Unique Carbons"] <= max_carbons)
+        # & (data["Synthetic Accessibility Score"] > data["Synthetic Accessibility Score"].quantile(1 - synthetic_quantile))
+    ]
 
-        # Filter top 5 candidates based on a chosen similarity metric
-        similarity_metric = st.selectbox(
-            "Select similarity metric to filter by:",
-            ("IR Similarity", "C-NMR Similarity", "H-NMR Similarity", "Similarity", "Sum of Similarities", "C-NMR IR Similarity", "H-NMR IR Similarity", "C-NMR H-NMR Similarity", "Tanimoto Similarity"),
+    # Filter top 5 candidates based on a chosen similarity metric
+    similarity_metric = st.selectbox(
+        "Select similarity metric to filter by:",
+        (
+            "IR Similarity",
+            "C-NMR Similarity",
+            "H-NMR Similarity",
+            "Similarity",
+            "Sum of Similarities",
+            "C-NMR IR Similarity",
+            "H-NMR IR Similarity",
+            "C-NMR H-NMR Similarity",
+            "Tanimoto Similarity",
+        ),
+    )
+
+    top_n = st.number_input(
+        "Select number of top candidates:",
+        min_value=1,
+        max_value=len(df_filtered),
+        value=5,
+    )
+    # if length of df_filtered is less than top_n, set top_n to length of df_filtered
+    top_n = min(top_n, len(df_filtered))
+    filtered_df = df_filtered.sort_values(
+        by=similarity_metric, ascending=False
+    ).head(top_n)
+
+    # Find the molecule with Tanimoto similarity of 1
+
+    # Directory to save images
+    image_dir = "molecule_images"
+    os.makedirs(image_dir, exist_ok=True)
+
+    # with contextlib.suppress(Exception):
+    if not tanimoto_1_df.empty:
+        st.subheader("Correct molecule from spectra")
+        for index, row in tanimoto_1_df.iterrows():
+            image_path = os.path.join(image_dir, f"tanimoto_1_{index}.png")
+            save_molecule_image(row["SMILES"], image_path)
+            st.image(image_path)
+    else:
+        st.subheader(
+            "No molecule with Tanimoto Similarity of 1 found in the dataset."
         )
 
-        top_n = st.number_input("Select number of top candidates:", min_value=1, max_value=len(df_filtered), value=5)
-        # if length of df_filtered is less than top_n, set top_n to length of df_filtered
-        top_n = min(top_n, len(df_filtered))
-        filtered_df = df_filtered.sort_values(by=similarity_metric, ascending=False).head(top_n)
+    # Display top candidates
+    st.subheader(f"Top {top_n} Candidates by {similarity_metric}")
 
-        # Find the molecule with Tanimoto similarity of 1
-        tanimoto_1_df = df_filtered[df_filtered["Tanimoto Similarity"] == 1]
+    # display images in columns
+    col1, col2, col3 = st.columns(3)
+    # assign columns per image
+    nr_of_rows = top_n // 3 + 1
+    column_list = []
+    for _ in range(nr_of_rows):
+        column_list.extend([col1, col2, col3])
+    for (index, row), col in zip(filtered_df.iterrows(), column_list):
+        with col:
+            image_path = os.path.join(image_dir, f"mol_{index}.png")
+            save_molecule_image(row["SMILES"], image_path)
+            st.image(
+                image_path,
+                caption=f"SMILES:\n{row['SMILES']}\n{similarity_metric}: {row[similarity_metric]:.2f}\nTanimoto Similarity: {row['Tanimoto Similarity']:.2f}",
+                use_column_width=True,
+            )
 
-        # Directory to save images
-        image_dir = "molecule_images"
-        os.makedirs(image_dir, exist_ok=True)
-
-    with contextlib.suppress(Exception):
-        if not tanimoto_1_df.empty:
-            st.subheader("Correct molecule from spectra")
-            for index, row in tanimoto_1_df.iterrows():
-                image_path = os.path.join(image_dir, f"tanimoto_1_{index}.png")
-                save_molecule_image(row["SMILES"], image_path)
-                st.image(image_path)
-        else:
-            st.subheader("No molecule with Tanimoto Similarity of 1 found in the dataset.")
-
-    with contextlib.suppress(Exception):
-        # Display top candidates
-        st.subheader(f"Top {top_n} Candidates by {similarity_metric}")
-
-        # display images in columns
-        col1, col2, col3 = st.columns(3)
-        # assign columns per image
-        nr_of_rows = top_n // 3 + 1
-        column_list = []
-        for _ in range(nr_of_rows):
-            column_list.extend([col1, col2, col3])
-        for (index, row), col in zip(filtered_df.iterrows(), column_list):
-            with col:
-                image_path = os.path.join(image_dir, f"mol_{index}.png")
-                save_molecule_image(row["SMILES"], image_path)
-                st.image(image_path, caption=f"{similarity_metric}: {row[similarity_metric]:.2f}")
-
-        # Option to display raw data
-        if st.checkbox("Show Raw Data"):
-            st.write(data)
 else:
     st.write("Please upload a CSV file.")
