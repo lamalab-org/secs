2f1e17c4edd1a2324a7f10b767f2cb6cde3a3550
AdrianM0 <adrianmirza81@gmail.com>
2024-08-30 12:59:40 +0200

    lint

--- app/visualize_results/app_res_viz.py
+++ app/visualize_results/app_res_viz.py
@@ -12,7 +12,11 @@ def find_original_smiles_from_file_name(file_name):
 
 def determine_if_the_molecule_with_sim_1_in_top_n(sorted_df, n, return_average=False):
     tanimoto_top_n = sorted_df.head(n)["Tanimoto Similarity"]
-    return average_tanimoto_of_top_n(sorted_df, n) if return_average else int(1 in tanimoto_top_n.to_list())
+    return (
+        average_tanimoto_of_top_n(sorted_df, n)
+        if return_average
+        else int(1 in tanimoto_top_n.to_list())
+    )
 
 
 def average_tanimoto_of_top_n(sorted_df, n):
@@ -84,16 +88,24 @@ def app():
     read_all_dataframe = [pd.read_csv(file) for file in result_files]
     st.write(f"All files: {len(read_all_dataframe)}")
     # filter if Tanimoto 1 not in column
-    read_all_dataframe = [df for df in read_all_dataframe if len(df[df["tanimoto"] == 1]) == 1]
-    read_all_dataframe = [df for df in read_all_dataframe if len(df) > 10*top_n]
+    read_all_dataframe = [
+        df for df in read_all_dataframe if len(df[df["tanimoto"] == 1]) == 1
+    ]
+    read_all_dataframe = [df for df in read_all_dataframe if len(df) > 10 * top_n]
     # select box for metric to calculate
-    average_or_not = st.checkbox(f"Average Maximum Tanimoto similarity from Top-{top_n}", value=False)
+    average_or_not = st.checkbox(
+        f"Maximum Tanimoto similarity from Top-{top_n}", value=False
+    )
     # select box for adding nmr peaks or not
     add_nmr_peaks = st.checkbox("Add NMR Peaks", value=False)
     # plot molecule size vs performance for each metric
-    plot_molecule_size_vs_performance = st.checkbox("Plot Molecule Size vs Performance", value=False)
+    plot_molecule_size_vs_performance = st.checkbox(
+        "Plot Molecule Size vs Performance", value=False
+    )
     # plot metrics vs how many molecules are in each csv file
-    plot_metrics_vs_number_of_isomers = st.checkbox("Plot Metrics vs Number of Isomers", value=False)
+    plot_metrics_vs_number_of_isomers = st.checkbox(
+        "Plot Metrics vs Number of Isomers", value=False
+    )
     smiles_list = []
 
     if add_nmr_peaks:
@@ -104,7 +116,8 @@ def app():
             nr_of_unique_hydrogens = tanimoto_1["unique_hydrogens"].to_list()[0]
             nr_of_unique_carbons = tanimoto_1["unique_carbons"].to_list()[0]
             filtered_dataframe = dataframe[
-                (dataframe["unique_hydrogens"] == nr_of_unique_hydrogens) & (dataframe["unique_carbons"] == nr_of_unique_carbons)
+                (dataframe["unique_hydrogens"] == nr_of_unique_hydrogens)
+                & (dataframe["unique_carbons"] == nr_of_unique_carbons)
             ]
             read_all_dataframe[id_] = filtered_dataframe
     else:
@@ -113,13 +126,17 @@ def app():
             smiles_list.append(tanimoto_1["canonical_smiles"].to_list()[0])
     st.write(f"Files satisfying the conditions: {len(read_all_dataframe)}")
     # collect metrics for all files
-    metrics_list = [collect_statistics_from_files(df, to_collect_columns, top_n, average_or_not) for df in read_all_dataframe]
+    metrics_list = [
+        collect_statistics_from_files(df, to_collect_columns, top_n, average_or_not)
+        for df in read_all_dataframe
+    ]
     # plot len(molecules) vs performance for each metric
     import seaborn as sns
+
     # white background
     sns.set_style("whitegrid")
     # no grid
-    sns.set_style("whitegrid", {'axes.grid': False})
+    sns.set_style("whitegrid", {"axes.grid": False})
     # keep only x and y axis
     # fig, ax = plt.subplots(nrows=4, ncols=2, figsize=(10, 20))
     # for i, column in enumerate(to_collect_columns):
@@ -144,38 +161,61 @@ def app():
 
     if not average_or_not:
         st.subheader(f"Is in Top-{top_n}")
-        st.write(mean.T.sort_values(by=f"Top {top_n}", axis=1, ascending=False).style.highlight_max(axis=1))
+        st.write(
+            mean.T.sort_values(
+                by=f"Top {top_n}", axis=1, ascending=False
+            ).style.highlight_max(axis=1)
+        )
 
     if average_or_not:
-        st.subheader(f"Max Tanimoto Similarity of Top-{top_n}")
-        st.write(mean.T.sort_values(by=f"Top {top_n}", axis=1, ascending=False).style.highlight_max(axis=1))
+        st.subheader(f"Average Max Tanimoto Similarity of Top-{top_n}")
+        st.write(
+            mean.T.sort_values(
+                by=f"Top {top_n}", axis=1, ascending=False
+            ).style.highlight_max(axis=1)
+        )
 
     len_df = [len(df) for df in read_all_dataframe]
     if plot_molecule_size_vs_performance:
+        if average_or_not:
+            st.write("TS = Tanimoto Similarity")
+
         fig, ax = plt.subplots(nrows=4, ncols=2, figsize=(8, 15), dpi=200)
         for i, column in enumerate(to_collect_columns):
             column_values = [metrics[column] for metrics in metrics_list]
-            sns.scatterplot(x=smiles_list_lengths, y=column_values, s=40, ax=ax[i // 2, i % 2], c="g")
-            # ax[i // 2, i % 2].set_title(column)
-            # ax[i // 2, i % 2].set_xticks(smiles_list_lengths)
-            ax[i // 2, i % 2].set_ylabel("Tanimoto Similarity" if average_or_not else f"Is in {top_n} ({column})")
+            sns.scatterplot(
+                x=smiles_list_lengths,
+                y=column_values,
+                s=40,
+                ax=ax[i // 2, i % 2],
+                c="g",
+            )
+            ax[i // 2, i % 2].set_ylabel(
+                f"Most TS in {top_n} ({column})"
+                if average_or_not
+                else f"Is in {top_n} ({column})"
+            )
         # set one x label for all subplots
-        fig.text(0.5, 0.07, "Molecule Size", ha="center")
+        fig.text(0.5, 0.08, "Molecule Size", ha="center")
 
         st.pyplot(fig)
     if plot_metrics_vs_number_of_isomers:
         # plot metrics vs how many molecules are in each csv file
         # add comment
         st.subheader("Plotting metrics vs number of isomers for each file")
-        fig, ax = plt.subplots(nrows=4, ncols=2, figsize=(5, 15), dpi=200)
+        fig, ax = plt.subplots(nrows=4, ncols=2, figsize=(8, 15), dpi=200)
         for i, column in enumerate(to_collect_columns):
             column_values = [metrics[column] for metrics in metrics_list]
-            # log scale for x-axis
-            sns.scatterplot(x=len_df, y=column_values,ax=ax[i // 2, i % 2], c="r", s=40)
+            sns.scatterplot(
+                x=len_df, y=column_values, ax=ax[i // 2, i % 2], c="r", s=40
+            )
             ax[i // 2, i % 2].set_xscale("log")
-            ax[i // 2, i % 2].set_xlabel("Number of Isomers")
-            ax[i // 2, i % 2].set_ylabel("Tanimoto Similarity" if average_or_not else f"Is in Top-{top_n} ({column})")
-        fig.text(0.5, 0.04, "Number of Isomers", ha="center")
+            ax[i // 2, i % 2].set_ylabel(
+                f"Max Tanimoto Similarity in {top_n}"
+                if average_or_not
+                else f"Is in Top-{top_n} ({column})"
+            )
+        fig.text(0.5, 0.08, "Number of Isomers", ha="center")
         st.pyplot(fig)
 
     # plot molecule size vs number of isomers
@@ -185,6 +225,7 @@ def app():
     ax.set_ylabel("Number of Isomers")
     ax.set_yscale("log")
     st.pyplot(fig)
+
 
 if __name__ == "__main__":
     app()
